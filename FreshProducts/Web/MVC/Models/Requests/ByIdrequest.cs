66a4c30776e277d4d0010a53d90784637e4eb02b
Happy <kolesnikjenya8@gmail.com>
2024-04-21 16:29:38 +0300

    check

---
+++ FreshProducts/Web/MVC/Models/Requests/ByIdrequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models.Reqeusts
+{
+	public class ByIdrequest
+	{
+		[Required]
+		[Range(0, int.MaxValue, ErrorMessage = "The field must be greater than or equal to 0.")]
+		public int Id { get; set; }
+	}
+}
